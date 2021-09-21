c876066c671c433509a6e3bc8c228fea1e4608e1
Rafael Bressan <bressan.rafa@gmail.com>
2021-09-21 15:31:56 -0300

    Changed name of Context.Text.OnSurface.Base to Main, Context.Text.OnBackground.Base to Main, and Context.Text.OnColor.Base to Main. It helps differentiate from Color.Base, and other Base aliases we use on the tokens.

--- config.js
+++ config.js
@@ -1,94 +1,4 @@
-const StyleDictionary = require("style-dictionary").extend({
-  source: ["tokens/**/*.json"],
-  platforms: {
-    js: {
-      transforms:['attribute/cti', 'name/cti/camel', 'size/object', 'color/css'],
-      buildPath: "build/react-native/styleguide/",
-      files: [
-        {
-            name: "colorsBase",
-            destination: "colorsBase.js",
-            format: "javascript/object",
-            options: {
-              outputReferences: true,
-              fileHeader: "tokenHeader",
-            },
-            filter: "isColorBase",
-          },
-          {
-            name: "colorsContext",
-            destination: "colorsContext.js",
-            format: "javascript/object",
-            options: {
-              outputReferences: true,
-              fileHeader: "tokenHeader",
-            },
-            filter: "isColorContext",
-          },
-          {
-            name: "fonts",
-            destination: "fonts.js",
-            format: "javascript/object",
-            options: {
-              outputReferences: true,
-              fileHeader: "tokenHeader",
-            },
-            filter: "isFont",
-          },
-          {
-            name: "opacities",
-            destination: "opacities.js",
-            format: "javascript/object",
-            options: {
-              outputReferences: true,
-              fileHeader: "tokenHeader",
-            },
-            filter: "isOpacity",
-          },
-          {
-            name: "radius",
-            destination: "radius.js",
-            format: "javascript/object",
-            options: {
-              outputReferences: true,
-              fileHeader: "tokenHeader",
-            },
-            filter: "isRadius",
-          },
-          {
-            name: "shadows",
-            destination: "shadows.js",
-            format: "javascript/object",
-            options: {
-              outputReferences: true,
-              fileHeader: "tokenHeader",
-            },
-            filter: "isShadow",
-          },
-          {
-            name: "sizes",
-            destination: "sizes.js",
-            format: "javascript/object",
-            options: {
-              outputReferences: true,
-              fileHeader: "tokenHeader",
-            },
-            filter: "isSize",
-          },
-          {
-            name: "spacings",
-            destination: "spacings.js",
-            format: "javascript/object",
-            options: {
-              outputReferences: true,
-              fileHeader: "tokenHeader",
-            },
-            filter: "isSpacing",
-          },
-      ],
-    },
-  },
-});
+const StyleDictionary = require("style-dictionary");
 
 //Header
 
@@ -97,64 +7,183 @@ StyleDictionary.registerFileHeader({
   fileHeader: function (defaultMessage) {
     return [...defaultMessage, `Made by Pagar.me BaaS`];
   },
-});
+}),
+  //Filters
 
-//Filters
-
-StyleDictionary.registerFilter({
-  name: "isSpacing",
-  matcher: function (token) {
-    return token.attributes.category === "spacing";
-  },
-});
-
-StyleDictionary.registerFilter({
-  name: "isOpacity",
-  matcher: function (token) {
-    return token.type === "opacity"
-  },
-});
-
-StyleDictionary.registerFilter({
-  name: "isRadius",
-  matcher: function (token) {
-    return token.type === "custom-radius"
-  },
-});
-
-StyleDictionary.registerFilter({
+  StyleDictionary.registerFilter({
+    name: "isSpacing",
+    matcher: function (token) {
+      return token.attributes.category === "spacing";
+    },
+  }),
+  StyleDictionary.registerFilter({
+    name: "isOpacity",
+    matcher: function (token) {
+      return token.type === "opacity";
+    },
+  }),
+  StyleDictionary.registerFilter({
+    name: "isRadius",
+    matcher: function (token) {
+      return token.type === "custom-radius";
+    },
+  }),
+  StyleDictionary.registerFilter({
     name: "isColorBase",
     matcher: function (token) {
-      return (token.attributes.category === "color" && token.attributes.type === "base") || token.attributes.type === "neutral"
+      return (
+        (token.attributes.category === "color" &&
+          token.attributes.type === "base") ||
+        token.attributes.type === "neutral"
+      );
     },
-  });
-
+  }),
   StyleDictionary.registerFilter({
     name: "isColorContext",
     matcher: function (token) {
-      return token.attributes.type === "context"
+      return token.attributes.type === "context";
     },
-  });
-
+  }),
   StyleDictionary.registerFilter({
     name: "isFont",
     matcher: function (token) {
-      return token.type === "custom-fontStyle"
+      return token.type === "custom-fontStyle";
     },
-  });
-
+  }),
   StyleDictionary.registerFilter({
     name: "isShadow",
     matcher: function (token) {
-      return token.type === "custom-shadow"
+      return token.type === "custom-shadow";
     },
-  });
-
+  }),
   StyleDictionary.registerFilter({
     name: "isSize",
     matcher: function (token) {
-      return token.type === "dimension"
+      return token.type === "dimension";
     },
   });
 
-StyleDictionary.buildAllPlatforms();
+//Styleguide Tokens
+
+StyleDictionary.extend({
+  source: ["tokens/**/*.json"],
+  platforms: {
+    js: {
+      transforms: [
+        "attribute/cti",
+        "name/cti/camel",
+        "size/object",
+        "color/css",
+      ],
+      buildPath: "build/react-native/styleguide/",
+      files: [
+        {
+          name: "colorsBase",
+          destination: "colorsBase.js",
+          format: "javascript/object",
+          options: {
+            outputReferences: true,
+            fileHeader: "tokenHeader",
+          },
+          filter: "isColorBase",
+        },
+        {
+          name: "colorsContext",
+          destination: "colorsContext.js",
+          format: "javascript/object",
+          options: {
+            outputReferences: true,
+            fileHeader: "tokenHeader",
+          },
+          filter: "isColorContext",
+        },
+        {
+          name: "fonts",
+          destination: "fonts.js",
+          format: "javascript/object",
+          options: {
+            outputReferences: true,
+            fileHeader: "tokenHeader",
+          },
+          filter: "isFont",
+        },
+        {
+          name: "opacities",
+          destination: "opacities.js",
+          format: "javascript/object",
+          options: {
+            outputReferences: true,
+            fileHeader: "tokenHeader",
+          },
+          filter: "isOpacity",
+        },
+        {
+          name: "radius",
+          destination: "radius.js",
+          format: "javascript/object",
+          options: {
+            outputReferences: true,
+            fileHeader: "tokenHeader",
+          },
+          filter: "isRadius",
+        },
+        {
+          name: "shadows",
+          destination: "shadows.js",
+          format: "javascript/object",
+          options: {
+            outputReferences: true,
+            fileHeader: "tokenHeader",
+          },
+          filter: "isShadow",
+        },
+        {
+          name: "sizes",
+          destination: "sizes.js",
+          format: "javascript/object",
+          options: {
+            outputReferences: true,
+            fileHeader: "tokenHeader",
+          },
+          filter: "isSize",
+        },
+        {
+          name: "spacings",
+          destination: "spacings.js",
+          format: "javascript/object",
+          options: {
+            outputReferences: true,
+            fileHeader: "tokenHeader",
+          },
+          filter: "isSpacing",
+        },
+      ],
+    },
+  },
+}).buildAllPlatforms();
+
+StyleDictionary.extend({
+  source: ["tokens/**/*.json"],
+  platforms: {
+    js: {
+      transforms: [
+        "attribute/cti",
+        "name/cti/camel",
+        "size/object",
+        "color/css",
+      ],
+      buildPath: "build/react-native/components/",
+      files: [
+        {
+          name: "button",
+          destination: "button.js",
+          format: "javascript/object",
+          options: {
+            outputReferences: true,
+            fileHeader: "tokenHeader",
+          },
+        },
+      ],
+    },
+  },
+}).buildAllPlatforms();
